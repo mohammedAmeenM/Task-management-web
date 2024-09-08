e32354d780ff54c564f814769b28a3135a8cd228
amiiiiee <amiameen507@gmail.com>
2024-09-08 20:15:19 +0530

    edit-api

--- src/axiosInterceptors.js
+++ src/axiosInterceptors.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL:"http://localhost:3000/api"
+    baseURL:"https://task-management-rest-apis.onrender.com/api"
 });
 api.interceptors.request.use(function (config) {
     const token =localStorage.getItem('token'); 
